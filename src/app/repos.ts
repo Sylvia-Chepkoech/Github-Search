ba10b613dae1f6133a00c208eab28b69306657a3
Sylvia-Chepkoech <slycheche@gmail.com>
2022-04-20 00:38:26 +0300

    generate navbar component

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -1,2 +1,12 @@
 export class Repos {
+  // constructor(
+  //   public homepage: string,
+  //   public html_url: string,
+  //   public full_name: string,
+  //   public name: string,
+  //   public forks_count: number,
+  //   public subscribers_count: number,
+  //   public description: string,
+  //   public created_at: Date
+  // )
 }
