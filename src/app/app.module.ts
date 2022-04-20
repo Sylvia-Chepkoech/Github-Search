0806beb6327ecefb24f16ba6fb4128d5a4cdc455
Sylvia-Chepkoech <slycheche@gmail.com>
2022-04-20 06:51:37 +0300

    generate not-found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { GitprofileService } from './gitprofile-service/gitprofile.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
